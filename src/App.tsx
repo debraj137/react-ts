812e26c2bca090bcaceeb27db0f6186fed2b0d41
debrajdeb137 <debrajdeb137@gmail.com>
2026-02-15 15:09:18 +0530

    button and typography use in material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // import { useState } from 'react'
 // import { useEffect, useRef } from 'react'
 // import { useState } from 'react'
+import { Button, Typography } from '@mui/material';
 import './App.css'
-import List from './List';
+// import List from './List';
 // import Child from './Child'
 
 function App() {
@@ -18,12 +19,16 @@ function App() {
   //   setEmail(e.target.value)
   // }
   return (
-    // <Child name='debraj' age={30}/>
-    // <button onClick={()=>setCount(count+1)}>{count}</button>
-    // <input type="text" ref = {inputRef}/>
-    // <input type="text" value={email} onChange={handleChange} placeholder='enter your email'/>
+    <>
+      {/* <Child name='debraj' age={30} />
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <input type="text" ref={inputRef} />
+      <input type="text" value={email} onChange={handleChange} placeholder='enter your email' />
 
-    <List items={[1,2,3,4,5]} render={(item)=><p key={item}>{item}</p>}/>
+      <List items={[1, 2, 3, 4, 5]} render={(item) => <p key={item}>{item}</p>} /> */}
+      <Typography variant='h4'>Welcome</Typography>
+      <Button>Click Me</Button>
+    </>
   )
 }
 
