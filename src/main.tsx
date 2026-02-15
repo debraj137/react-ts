97eac7cb05ccde9854a766d0593864483823c438
debrajdeb137 <debrajdeb137@gmail.com>
2026-02-15 15:55:58 +0530

    Wrap with ThemeProvider and you get full dark mode support

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from '@mui/material'
-import { theme } from './theme.ts'
+import { darkTheme } from './theme.ts'
+// import { theme } from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
   //   <App />
   // </StrictMode>,
-  <ThemeProvider theme={theme}>
-    <App/>
+  // <ThemeProvider theme={theme}>
+  //   <App/>
+  // </ThemeProvider>
+  <ThemeProvider theme={darkTheme}>
+    <App />
   </ThemeProvider>
 )
--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,22 @@
 import { createTheme } from "@mui/material";
 
-export const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#1e88e5",
-        },
-        secondary: {
-            main: "#ff4081",
-        }
-    },
-    typography: {
-        fontFamily: "Arial"
-    }
-})
+// export const theme = createTheme({
+//     palette: {
+//         primary: {
+//             main: "#1e88e5",
+//         },
+//         secondary: {
+//             main: "#ff4081",
+//         }
+//     },
+//     typography: {
+//         fontFamily: "Arial"
+//     }
+// })
+
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
