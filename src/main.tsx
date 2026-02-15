6c4f64c5d0b8da90ccd7e2263405d6ce3b95a5c7
debrajdeb137 <debrajdeb137@gmail.com>
2026-02-15 15:51:31 +0530

    use MUI’s ThemeProvider to maintain design consistency across the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <ThemeProvider theme={theme}>
+    <App/>
+  </ThemeProvider>
 )
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#1e88e5",
+        },
+        secondary: {
+            main: "#ff4081",
+        }
+    },
+    typography: {
+        fontFamily: "Arial"
+    }
+})
